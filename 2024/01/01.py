52cb9f0f5b495210de94443e5ccd908c67905556
francisco.deborja <francisco.deborja@servexternos.repsol.com>
2025-10-24 14:33:13 +0200

    almost done

--- 2024/01/01.py
+++ 2024/01/01.py
@@ -1,4 +1,7 @@
 import os 
+import numpy as np
+import pandas as pd
+
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 input_file = os.path.join(script_dir, "input_file.txt")
@@ -10,6 +13,6 @@ with open(input_file, "r", encoding="utf-8") as f:
         locs_1.append(int(locs[0]))
         locs_2.append(int(locs[1]))
 
-print(locs_1, locs_2)
-        
-        
+
+locs_1, locs_2 = np.sort(np.array(locs_1)), np.sort(np.array(locs_2))
+print(locs_1, locs_2, abs(np.sum(locs_1-locs_2)))
